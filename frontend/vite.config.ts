3710a5544de9317031e7b2124959c95a0fb55532
9thday <148792346+s3bhi@users.noreply.github.com>
2025-11-04 21:54:59 +0530

    Update vite.config.ts

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,6 +2,14 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+export default defineConfig({
+  plugins: [react()],
+  base: "/fraud_card_detector/", // 👈 repo name
+});
+
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
