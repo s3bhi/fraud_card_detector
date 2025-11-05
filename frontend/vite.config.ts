9de565300dacf23dadfdaccfeca36d2147247c0d
Prety2910 <sharmaprety99@gmail.com>
2025-11-05 08:52:03 +0530

    Update vite.config.ts

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,1 +1,23 @@
-..
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import path from "path";
+import { componentTagger } from "lovable-tagger";
+
+export default defineConfig(({ mode }) => ({
+  server: {
+    host: "::",
+    port: 8080,
+  },
+  // ✅ Important for GitHub Pages
+  // On local: base = "/" (normal)
+  // On GitHub Pages: base = "/<repo-name>/"
+  base: mode === "development" ? "/" : "/fraud_card_detector/",
+
+  plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
+
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+}));
